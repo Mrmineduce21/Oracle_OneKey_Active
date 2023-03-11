da5673c2b9c4f8b33bc1953f9f24c9ec81fb8f86
Mrmineduce21 <100993460+Mrmineduce21@users.noreply.github.com>
2023-03-11 20:03:31 +0800

    Update OneKey_FuckMemory.sh

--- OneKey_FuckMemory.sh
+++ OneKey_FuckMemory.sh
@@ -4,7 +4,7 @@ red='\033[0;31m'
 green='\033[0;32m'
 yellow='\033[0;33m'
 plain='\033[0m'
-G="G"
+
 cur_dir=$(pwd)
 
 
@@ -37,42 +37,46 @@ install_base() {
     echo -e "${yellow}install_base${plain} "
     if [[ x"${release}" == x"centos" ]]; then
         yum install epel-release -y
-        yum install wget socat -y
+        yum install wget socat bc -y
     else
-    apt-get install wget socat -y
+    apt-get install wget socat bc -y
     fi
 }
 
 
 Fuck_Memory() {
-freeMem=$(free -t | awk 'NR ==2 {print $4/1000/1000}')
-TCMem=$(free -t | awk 'NR ==2 {print $4/1000/1000/10}') && full=$((${TCMem//.*/+1})) 
-   fullG=${full}$G
-   echo -e "${green}当前获取到的可用内存为_${freeMem}GB ${plain}"
-  
-   echo -e "${green}预计填充的内存为_${fullG} ${plain}"
-  
-   echo -e "${green}===============================${plain} "
-   echo -e "${yellow}  脚本将自动获取系统可用内存的10%（不满1G按1G算） 并消耗掉这部分内存 ${plain} "
-   echo -e "${red}  警告：Amd服务器（内存小于2G）请勿使用此脚本，对此照成的封号，机器失联，卡死等问题作者概不负责 ${plain} "
-   echo -e "${red}  输入数字14运行即表示同意 ${plain} "
-   read -p "请输入: " -t 30 rules
 
-if (( $rules == 14 )); then
-    echo -e "${green}  开始运行 ${plain} "
-       echo -e "${fullG}"
-    else
-  echo -e "${red}不同意别用！${plain}\n" && exit 1
+
+
+# 获取系统内存总量和当前已用内存
+total_mem=$(free | awk '/Mem:/ {print $2}')
+used_mem=$(free | awk '/Mem:/ {print $3}')
+
+# 计算当前内存占用率，保留小数点后两位
+used_percent=$(awk "BEGIN {printf \"%.2f\", $used_mem/$total_mem*100}")
+
+# 判断内存占用率是否小于10%
+if [ $(echo "$used_percent < 10" | bc) -eq 1 ]; then
+    # 计算内存占用距离15%差多少MB，保留整数位
+    mem_diff=$(awk "BEGIN {print int(($total_mem*0.15-$used_mem)/1024)}")
+    echo "当前内存占用率为${used_percent}%，距离15%还差${mem_diff}MB"
+    FullM="${mem_diff}M"
+        if [ -f /root/memory_usage.sh ]; then
+             echo "file found...开始执行 吃掉内存"
+                cd /root && wget -qO memory_usage.sh https://raw.githubusercontent.com/Mrmineduce21/Oracle_OneKey_Active/main/memory_usage.sh && chmod +x memory_usage.sh && bash memory_usage.sh consume ${FullM}
+        else
+                echo "no such file!-开始下载"
+                cd /root && wget -qO memory_usage.sh https://raw.githubusercontent.com/Mrmineduce21/Oracle_OneKey_Active/main/memory_usage.sh && chmod +x memory_usage.sh && bash memory_usage.sh consume ${FullM}
+        fi
+else
+    echo -e "${green}内存占用达到15%${plain}"
+    exit 1
 fi
 
-#运行部分
-if [ -f /root/memory_usage.sh ]; then
- echo "file found...开始执行 吃掉内存"
-cd /root && wget -qO memory_usage.sh https://raw.githubusercontent.com/Mrmineduce21/Oracle_OneKey_Active/main/memory_usage.sh && chmod +x memory_usage.sh && bash memory_usage.sh consume 2G
-  else
-  echo "no such file!-开始下载"
-cd /root && wget -qO memory_usage.sh https://raw.githubusercontent.com/Mrmineduce21/Oracle_OneKey_Active/main/memory_usage.sh && chmod +x memory_usage.sh && bash memory_usage.sh consume 2G
-fi
+
+
+
+
 
 }
 
